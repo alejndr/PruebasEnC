88ad077faf6d4da5aafab9e806107fd0862b537b
Alejandro <a@m.c>
2018-05-09 10:19:23 +0200

    Introducido metodos genericos

--- Generics/Generics.cs
+++ Generics/Generics.cs
@@ -10,6 +10,37 @@ namespace Generics
     {
         static void Main(string[] args)
         {
+            int numeros = 123;
+
+            String Palabras = "blablabla";
+
+            double Decimales = 1.56;
+
+
+            // Prueba con int
+            MetodoGenerico<int>(numeros); // entre <> podemos indicar el tipo que queremos que use el metodo.
+                                          // Intellisense nos indica que podemos simplificarlo, como se muestra en lso siguientes.
+            // Prueba con string
+            MetodoGenerico(Palabras);
+
+            // Prueba con double
+            MetodoGenerico(Decimales);
+
+            Console.Read();
+
+
+        }
+
+        /// <summary>
+        /// Metodo generico que imprime lo que se le pase, independientemente de su tipo
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mensaje"></param>
+        public static void MetodoGenerico<T>(T mensaje)
+        {
+            
+           Console.WriteLine(mensaje);
+            
         }
     }
 }
