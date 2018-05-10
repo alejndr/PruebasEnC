cf12afef23a1e5c58f2e0880b74b1bbd088305a6
Alejandro <a@m.c>
2018-05-10 13:15:28 +0200

    Añadida secuencia fibonacci

---
+++ SecuenciaFibonacci/SecuenciaFibonacci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecuenciaFibonacci
+{
+    class SecuenciaFibonacci
+    {
+        static void Main(string[] args)
+        {
+            int prev = -1;
+            int next = 1;
+
+            for (int i = 0; i < 20; i++)
+            {
+                int sum = prev + next;
+                prev = next;
+                next = sum;
+                Console.WriteLine(sum);
+            }
+
+            Console.Read();
+        }
+    }
+}
