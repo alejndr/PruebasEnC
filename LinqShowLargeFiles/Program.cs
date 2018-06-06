1d14122912a301ed1eaee21594aabd35e294fa31
Alejandro <a@m.c>
2018-06-06 10:09:15 +0200

    Añadido LinqShowLargeFiles

---
+++ LinqShowLargeFiles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqShowLargeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows";
+            ShowLargeFilesWithLinq(path);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Con Linq obtenemos los 5 archivos mas grandes del path que le metamos
+        /// </summary>
+        /// <param name="path"></param>
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var query = new DirectoryInfo(path).GetFiles()
+                        .OrderByDescending(f => f.Length)
+                        .Take(5);
+
+            foreach (var file in query) //cogemos solo los 5 primeros
+            {
+                Console.WriteLine($"{file.Name,-15} : {file.Length}");
+            }
+
+            
+
+
+        }
+    }
+}
